01c7f485955869c5b97d11fc5d3b7c35406052ad
Kassio Schaider <kassio.schaider@hotmail.com>
2019-11-03 19:47:05 -0300

    Encapsulando métodos da classe Negociacoes e usando ela no Controller

--- app/ts/controllers/NegociacaoController.ts
+++ app/ts/controllers/NegociacaoController.ts
@@ -3,6 +3,7 @@ class NegociacaoController {
     private _inputData: HTMLInputElement;
     private _inputQuantidade: HTMLInputElement;
     private _inputValor: HTMLInputElement;
+    private _negociacoes = new Negociacoes();
 
     constructor() {
 
@@ -21,6 +22,6 @@ class NegociacaoController {
             parseFloat(this._inputValor.value)
         )
 
-        console.log(negociacao);
+        this._negociacoes.adiciona(negociacao);
     }
 }
--- app/ts/models/Negociacoes.ts
+++ app/ts/models/Negociacoes.ts
@@ -2,13 +2,13 @@ class Negociacoes {
 
     private _negociacoes: Array<Negociacao> = [];
 
-    adiciona(negociacao: Negociacao) {
+    adiciona(negociacao: Negociacao): void {
 
         this._negociacoes.push(negociacao);
     }
 
-    paraArray() {
+    paraArray(): Array<Negociacao> {
 
-       return this._negociacoes;
+       return [].concat(this._negociacoes);
     }
 }
